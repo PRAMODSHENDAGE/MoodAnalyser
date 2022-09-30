76478fdb6b7faf4b6f3be1836965815fce9ca8f0
KajalShendage <shendagekajal123@gmail.com>
2022-09-30 23:05:54 +0530

    Add project files.

---
+++ MoodAnalyzerProblem/MoodAnalysisException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+     //UC 3
+    public class MoodAnalysisException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MOOD, EMPTY_MOOD,
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD
+        }
+        public ExceptionType exceptionType;
+        public MoodAnalysisException(ExceptionType type, string message) : base(message)
+        {
+            this.exceptionType = type;
+        }
+    }
+}
---
+++ MoodAnalyzerProblem/MoodAnalyzer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        string message;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        public MoodAnalyzer()
+        {
+
+        }
+        /// <summary> UC 1
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// Analyses user's mood.
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <returns>Sad else Happy</returns>
+        public string AnalyseMood(string message)
+        {
+            ///Checks if user mood is sad will return sad,
+            ///if user mood is happy will return happy.
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+        //UC 2
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (message.Contains("SAD"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException ex)
+            {
+                return "HAPPY";
+            }
+        }
+        //UC 3
+        public string MoodAnalyse()
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(message))
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.EMPTY_MOOD, "Message is Empty");
+                }
+                if (message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (MoodAnalysisException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MOOD, "Message is ull");
+            }
+        }
+    }
+}
